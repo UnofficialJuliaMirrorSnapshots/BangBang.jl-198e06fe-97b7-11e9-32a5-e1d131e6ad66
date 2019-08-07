d4c4b1f7c7258acbe6d0005470aadc0ba3f46dae
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-07 10:34:38 +0000

    Snapshot of branch dev taken on 2019-08-07T06:34:37.499-04:00 from "https://github.com/UnofficialJuliaMirror/BangBang.jl-198e06fe-97b7-11e9-32a5-e1d131e6ad66"

--- test/__test_ir.jl
+++ test/__test_ir.jl
@@ -1,6 +1,3 @@
-display(Base.JLOptions())
-println()
-
 using InteractiveUtils: code_llvm
 using Test
 
@@ -21,7 +18,11 @@ twice(x) = 2x
     @test mapbb(twice, [1:3;], Val(false)) == [2, 4, 6]
 
     ir_noinbounds = llvm_ir(mapbb, (twice, Float64[], Val(false)))
-    @test nmatches(r"fmul <4 x double>", ir_noinbounds) == 0
+    if Base.JLOptions().check_bounds == 2  # --check-bounds=no
+        @test nmatches(r"fmul <4 x double>", ir_noinbounds) >= 4
+    else
+        @test nmatches(r"fmul <4 x double>", ir_noinbounds) == 0
+    end
     ir_inbounds = llvm_ir(mapbb, (twice, Float64[], Val(true)))
     @test nmatches(r"fmul <4 x double>", ir_inbounds) >= 4
 end
